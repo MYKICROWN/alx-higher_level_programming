c4c3d8df7b9305daf79440fe02ea8da4b8271579
Mikiyas Aklilu <mikiyasaklilu9@gmail.com>
2022-07-06 22:45:17 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,44 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {
-        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "DM": 900,
-        "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400
-    }
-    if not isinstance(roman_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    v = 0
-    c = 0
-    l = len(roman_string)
-    while c < l:
-        if c + 1 < l and roman_string[c:c + 2] in roman:
-            v += roman[roman_string[c:c + 2]]
-            c += 2
-        else:
-            v += roman[roman_string[c]]
-            c += 1
-    return v
+    r_nums = {
+        'MMM': 3000,
+        'MM': 2000,
+        'M': 1000,
+        'DCCC': 800,
+        'DCC': 700,
+        'DC': 600,
+        'CCC': 300,
+        'CC': 200,
+        'CD': 400,
+        'D': 500,
+        'CM': 900,
+        'LXXX': 80,
+        'LXX': 70,
+        'LX': 60,
+        'XXX': 30,
+        'XX': 20,
+        'XL': 40,
+        'L': 50,
+        'XC': 90,
+        'C': 100,
+        'VIII': 8,
+        'VII': 7,
+        'III': 3,
+        'II': 2,
+        'VI': 6,
+        'IV': 4,
+        'V': 5,
+        'IX': 9,
+        'X': 10,
+        'I': 1,
+
+    }
+    result = 0
+    for key in r_nums.keys():
+        if key in roman_string:
+            result += r_nums[key]
+            roman_string = roman_string.replace(key, '')
+
+    return result
